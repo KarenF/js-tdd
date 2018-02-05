10084574009c700657243bb59f0ca51f5f53dbef
KarenF <karenf@outlook.com>
2018-02-05 19:07:23 -0200

    add hook

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,22 +1,29 @@
 describe('App', () => {
-  describe('Method A', () => {
-    context('Case 1', () => {
-      it.skip('should happen lalala', () => {
+  var array
 
-      })
-    })
-
-    context('Case 2', () => {
-      it('should happen lalala', () => {
-
-      })
-      it('should happen mimimi', () => {
-
-      })
-    })
+  before(() => {
   })
 
-  describe('Method B', () => {
-
+  after(() => {
   })
+
+  beforeEach(() => {
+    array = [1, 2, 3]
+  })
+
+  afterEach(() => {
+  })
+
+  it('should have a size of 4 when push another value to the array', () => {
+    array.push(4)
+  })
+
+  it('should have a size of 2 when pop a value from the array', () => {
+    array.pop()
+  })
+
+  it('should remove the value 3 when use pop in the array', () => {
+    array.pop() === 3
+  })
+
 })
