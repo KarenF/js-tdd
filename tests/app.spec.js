6387e3a1a08f99a75ec3742d0914d715951ac523
KarenF <karenf@outlook.com>
2018-02-05 20:08:36 -0200

    add test

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -3,17 +3,12 @@ const expect = require('chai').expect
 describe('App', () => {
   let array
 
-  before(() => {
-  })
-
-  after(() => {
-  })
-
   beforeEach(() => {
     array = [1, 2, 3]
   })
 
-  afterEach(() => {
+  it('should be an array', () => {
+    expect(array).to.be.an('array')
   })
 
   it('should have a size of 4 when push another value to the array', () => {
@@ -23,9 +18,15 @@ describe('App', () => {
 
   it('should have a size of 2 when pop a value from the array', () => {
     array.pop()
+    expect(array).to.have.lengthOf(2)
   })
 
   it('should remove the value 3 when use pop in the array', () => {
-    array.pop() === 3
+    array.pop()
+    expect(array).to.not.include(3)
+  })
+
+  it('should return true when pop 3 from the array', () => {
+    expect(array.pop() === 3).to.be.true
   })
 })
