556bd29bc2d8f172b896009ba67ae11857d23569
KarenF <karenf@outlook.com>
2018-02-05 17:16:04 -0200

    add test

---
+++ tests/main.spec.js
@@ -0,0 +1,22 @@
+describe('App', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('should happen lalala', () => {
+
+      })
+    })
+
+    context('Case 2', () => {
+      it('should happen lalala', () => {
+
+      })
+      it('should happen mimimi', () => {
+
+      })
+    })
+  })
+
+  describe('Method B', () => {
+
+  })
+})
